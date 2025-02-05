933083bed9b9fec91427d31159a91b546f4c5afe
azamilink <zamiaswad@gmail.com>
2025-02-05 10:32:43 +0700

    shopping cart

--- js/script.js
+++ js/script.js
@@ -15,9 +15,17 @@ document.querySelector('#search-btn').onclick = (event) => {
     event.preventDefault();
 };
 
+// Toggle Class Active untuk shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-btn').onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+};
+
 // Klik diluar element
 const hm = document.querySelector('#hamburger-menu');
 const sb = document.querySelector('#search-btn');
+const sc = document.querySelector('#shopping-cart-btn');
 
 document.addEventListener('click', function (e) {
     if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -27,4 +35,32 @@ document.addEventListener('click', function (e) {
     if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
         searchForm.classList.remove('active');
     }
+
+    if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active');
+    }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-btn');
+
+itemDetailButtons.forEach((itemDetailBtn) => {
+    itemDetailBtn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    };
+});
+
+// klik tombol close
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+};
+
+// klik diluar modal
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none';
+    }
+};
